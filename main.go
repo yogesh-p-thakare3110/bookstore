7094181f71e8585df38a2cf6010a6cd986e43ab9
yogesh-p-thakare3110 <95970605+yogesh-p-thakare3110@users.noreply.github.com>
2022-03-11 14:53:26 +0530

    Add files via upload

---
+++ main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"log"
+	"net/http"
+
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/yogesh-p-thakare3110/router"
+)
+
+func main() {
+	r := router.RegisterBookStoreRoutes()
+	log.Fatal(http.ListenAndServe("localhost:9010", r))
+}
